169eadcfcb800c95b827bafc4def9f869fe4c1ad
Kame2021 <21cm0108@jec.ac.jp>
2022-12-15 20:34:58 +0900

    Initial Commit

---
+++ EventKit-Event/EventKit_EventApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventKit_EventApp.swift
+//  EventKit-Event
+//
+//  Created by Ryo on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct EventKit_EventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
